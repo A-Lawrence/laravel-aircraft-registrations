d86528e8d23c733e919d32aae9160911507f70a2
Anthony Lawrence <artisan@anthonylawrence.me.uk>
2018-03-27 20:18:40 +0100

    Added composer.

--- AircraftRegistration.php
+++ AircraftRegistration.php
@@ -3,7 +3,28 @@
 
 class AircraftRegistration
 {
+    /**
+     * @var array array of country codes (ISO 3166 2 letter) to formats.
+     */
+    protected $formats = [
+        "IE" => [
+            "^EI\-[A-Z]{3}$",
+            "^EJ\-[A-Z]{4}$",
+        ],
+        "GB" => [
+            "^G\-[A-Z]{4}$",
+        ],
+        "US" => [
+            "^N\-[1-9][0-9]{0,4}$",
+            "^N\-[1-9][0-9]{1,3}[A-Z]$",
+            "^N\-[1-9][0-9]{1,2}[A-Z][A-Z]$",
+        ],
+    ];
+
     public function __construct()
     {
+        //
     }
+
+
 }
